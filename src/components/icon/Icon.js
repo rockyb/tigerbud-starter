5ffd2593d325aa12654c9381e0bd222fb6837e24
juliana <juliana.leon@tigerspike.com>
2021-01-21 13:23:35 +0000

    fix icons SVG

--- src/components/icon/Icon.js
+++ src/components/icon/Icon.js
@@ -1,10 +1,6 @@
-import {color} from '@storybook/addon-knobs';
-import {size} from 'lodash';
 import React from 'react';
 import SvgIcon from 'react-native-svg-icon';
 import icons from '../../assets/svgs/icons';
-
-//import icons from '../../assets/svgs/icons';
 
 export const Icon = (props) => <SvgIcon {...props} svgs={icons} />;
 
