3ae0a18b8627adaf0b17cf9518f95e66b51e1886
juliana <juliana.leon@tigerspike.com>
2021-02-05 09:09:35 +0000

    fix asyncStorage

--- storybook/index.js
+++ storybook/index.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
-import { loadStories } from './storyLoader';
+import {AppRegistry} from 'react-native';
+import {getStorybookUI, configure} from '@storybook/react-native';
+import {loadStories} from './storyLoader';
 import './rn-addons';
 
 // import stories
@@ -11,7 +11,9 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
